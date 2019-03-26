e1f10ecfcfc31dca5fdd720b317d99f0dbbb5870
jergal1990 <jergal1990@gmail.com>
2019-03-26 15:57:30 +0500

    base version

---
+++ main.js
@@ -0,0 +1,11 @@
+appStoreButton.onclick = () => {
+	yaCounter51436042.reachGoal('appStore');
+	alert('Sorry! Application is not available in App Store yet =(');
+}
+
+googlePlayButton.onclick = () => {
+	yaCounter51436042.reachGoal('googlePlay');
+	location.href = 'https://play.google.com/store/apps/details?id=com.photoparad';
+}
+
+
